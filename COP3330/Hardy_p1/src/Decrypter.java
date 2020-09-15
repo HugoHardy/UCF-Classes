96e5456e3bf7d9538d449b955edf5474550ed61d
Hugo Hardy <hugoandre96@knight.ucf.edu>
2020-09-14 22:03:01 -0400

    creates switch function to implement cypher to decrypt the data

--- COP3330/Hardy_p1/src/Decrypter.java
+++ COP3330/Hardy_p1/src/Decrypter.java
@@ -2,13 +2,43 @@ import java.util.Arrays;
 
 public class Decrypter {
     public String decrypterMethod(String encrytedData){
-        int deconverterArray[] = new int[4];
+        int[] deconverterArray = new int[4];
         for(int i = 0; i <= 3; i++){
             String partOfString = encrytedData.substring(i, i+1); //gets each part of the array
             Integer x = Integer.valueOf(partOfString); //converts String input to integer
             //System.out.print("part:");
             //System.out.println(x);
-            deconverterArray[i] = (x+7)%10; //gets reaminder and adds them to the array
+            int cypher;
+            switch(deconverterArray[i]){ //determines value before finding remainder, keeping the original digit plus 7
+                case 7:
+                case 8: 
+                case 9:
+                    cypher = deconverterArray[i];
+                    break;
+                case 0:
+                    cypher = 10;
+                    break;
+                case 1:
+                    cypher = 11;
+                    break;
+                case 2:
+                    cypher = 12;
+                    break;
+                case 3:
+                    cypher = 13;
+                    break;
+                case 4:
+                    cypher = 14;
+                    break;
+                case 5:
+                    cypher = 15;
+                    break;
+                case 6:
+                     cypher = 16;
+                    break;
+                default:
+                    break;
+            }
             System.out.println(deconverterArray[i]);
         }
         int first, second, third, forth; //swaps the first and third int in the array
@@ -20,10 +50,9 @@ public class Decrypter {
         deconverterArray[2] = first;
         deconverterArray[1] = forth;
         deconverterArray[3] = second;
-        System.out.print("Your encrypted data is: ");
+        System.out.print("Your decrypted data is: ");
         String dencrytedData = Arrays.toString(deconverterArray);
-        encrytedData = encrytedData.replaceAll(", ", "").trim().substring(1, 5);
-        //encrytedData.replace(',',' ').replace('[', ' ').replace(']', ' ').trim();
+        encrytedData = encrytedData.replaceAll(", ", "").trim().substring(1, 5); //removes unnecessary characters from array to string conversion
         System.out.println(encrytedData);
         return encrytedData;
     }
