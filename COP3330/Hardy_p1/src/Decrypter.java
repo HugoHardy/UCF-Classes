aae83fc0f37e2c5d7a006aac2f7656da807dcec3
Hugo Hardy <hugoandre96@knight.ucf.edu>
2020-09-14 22:27:12 -0400

    bug fix: approperately renamed printed line from encryptedData to decrptedData in lines 51 to 53

--- COP3330/Hardy_p1/src/Decrypter.java
+++ COP3330/Hardy_p1/src/Decrypter.java
@@ -1,45 +1,42 @@
 import java.util.Arrays;
 
 public class Decrypter {
-    public String decrypterMethod(String encrytedData){
+    public void decrypterMethod(String encryptedData){
         int[] deconverterArray = new int[4];
         for(int i = 0; i <= 3; i++){
-            String partOfString = encrytedData.substring(i, i+1); //gets each part of the array
+            String partOfString = encryptedData.substring(i, i+1); //gets each part of the array
             Integer x = Integer.valueOf(partOfString); //converts String input to integer
-            //System.out.print("part:");
-            //System.out.println(x);
-            int cypher;
-            switch(deconverterArray[i]){ //determines value before finding remainder, keeping the original digit plus 7
+            switch(x){ //determines value before finding remainder, keeping the original digit plus 7
                 case 7:
-                case 8: 
+                case 8:
                 case 9:
-                    cypher = deconverterArray[i];
+                    deconverterArray[i] = x;
                     break;
                 case 0:
-                    cypher = 10;
+                    deconverterArray[i] = 10;
                     break;
                 case 1:
-                    cypher = 11;
+                    deconverterArray[i] = 11;
                     break;
                 case 2:
-                    cypher = 12;
+                    deconverterArray[i] = 12;
                     break;
                 case 3:
-                    cypher = 13;
+                    deconverterArray[i] = 13;
                     break;
                 case 4:
-                    cypher = 14;
+                    deconverterArray[i] = 14;
                     break;
                 case 5:
-                    cypher = 15;
+                    deconverterArray[i] = 15;
                     break;
                 case 6:
-                     cypher = 16;
+                    deconverterArray[i] = 16;
                     break;
                 default:
                     break;
             }
-            System.out.println(deconverterArray[i]);
+            deconverterArray[i] = deconverterArray[i]-7; //subtracts 7 from the reverse modulus numbers
         }
         int first, second, third, forth; //swaps the first and third int in the array
         first = deconverterArray[0];      //and the second and forth digit in the array
@@ -51,9 +48,8 @@ public class Decrypter {
         deconverterArray[1] = forth;
         deconverterArray[3] = second;
         System.out.print("Your decrypted data is: ");
-        String dencrytedData = Arrays.toString(deconverterArray);
-        encrytedData = encrytedData.replaceAll(", ", "").trim().substring(1, 5); //removes unnecessary characters from array to string conversion
-        System.out.println(encrytedData);
-        return encrytedData;
+        String decryptedData = Arrays.toString(deconverterArray);
+        decryptedData = decryptedData.replaceAll(", ", "").trim().substring(1, 5); //removes unnecessary characters from array to string conversion
+        System.out.println(decryptedData);
     }
 }
--- COP3330/Hardy_p1/src/Encrypter.java
+++ COP3330/Hardy_p1/src/Encrypter.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Encrypter {
     public static void main(String[] args) {
 
-        System.out.print("Would you kindly input 4 digit data: "); //kindly asks for an input
+        System.out.print("Would you kindly input 4 digit data?: "); //kindly asks for an input
         Scanner Input = new Scanner(System.in); //scans for String input
         String Data = Input.next();
         System.out.print("Your input: ");
@@ -12,16 +12,13 @@ public class Encrypter {
 
 
     }
-    public static String converterMethod(String data){ //method used to encrypt data
+    public static void converterMethod(String data){ //method used to encrypt data
 
-        int converterArray[] = new int[4];
+        int[] converterArray = new int[4];
         for(int i = 0; i <= 3; i++){
-            String partOfString = data.substring(i, i+1); //gets each part of the array
-            Integer x = Integer.valueOf(partOfString); //converts String input to integer
-            //System.out.print("part:");
-            //System.out.println(x);
-            converterArray[i] = (x+7)%10; //gets reaminder and adds them to the array
-            System.out.println(converterArray[i]);//inserts ints into converterArray
+            String partOfString = data.substring(i, i+1); //gets each char of the string
+            int x = Integer.parseInt(partOfString); //converts String input to individual integer
+            converterArray[i] = (x+7)%10; //gets remainder and adds them to the array
         }
         int first, second, third, forth; //swaps the first and third int in the array
         first = converterArray[0];      //and the second and forth digit in the array
@@ -38,6 +35,5 @@ public class Encrypter {
         System.out.println(encrytedData);
         Decrypter obj1 = new Decrypter(); //calls Decrypter class
         obj1.decrypterMethod(encrytedData); //transfers encrypted data to the decrypter class
-        return encrytedData;
     }
 }
