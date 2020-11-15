27692a9c8229e624992ad59a76578e6f0b4681f2
Hugo Hardy <hugoandre96@knight.ucf.edu>
2020-11-14 22:14:53 -0500

    Fixed: TaskList -> write: renaming of Item.getTaskname to Item.getTaskName

--- COP3330/Hardy_p4/src/TaskList.java
+++ COP3330/Hardy_p4/src/TaskList.java
@@ -45,7 +45,7 @@ public class TaskList {
         try(Formatter output = new Formatter(filename)) {
             for(int i = 0; i < Tasks.size(); i++) {
                 TaskItem Item = Tasks.get(i);
-                output.format("%s;%s%n", Item.getTaskname(), Item.getTaskDate());
+                output.format("%s;%s%n", Item.getTaskName(), Item.getTaskDate());
             }
 
         }
